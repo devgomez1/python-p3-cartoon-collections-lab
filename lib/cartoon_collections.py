3de97357fbbc57338e9c36ccbd35d8aa9636e196
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2024-01-02 10:03:56 -0600

    finished lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
+CHEESE = ["cheddar", "gouda", "camembert"]
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarfes):
+    for i in range(len(dwarfes)):
+        print(f"{i+1}. {dwarfes[i]}")
+    return dwarfes
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(elements):
+    for i in range(len(elements)):
+        elements[i] = elements[i].capitalize() + "!"
+    return elements
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(list):
+    for i in range(len(list)):
+        if len(list[i]) > 4:
+            return True
+    return False
+        
+
+def find_the_cheese(list):
+    for i in range(len(list)):
+        if list[i] in CHEESE:
+            return list[i]
+    return None
+
